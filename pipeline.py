fead1917f6a859c60fa82bacafd960600cb95b47
Shreyansh Gautam <shreygtm3@gmail.com>
2026-04-25 19:02:53 +0530

    Refactor ETL process into PowerPlantETL class

--- pipeline.py
+++ pipeline.py
@@ -1,80 +1,66 @@
 import pandas as pd
 import os
+import sys
+from sqlalchemy import create_engine
 
-# Memory Optimization
+class PowerPlantETL:
+    def __init__(self, config):
+        self.path = config['path']
+        self.db_uri = config['db_uri']
+        self.columns = ['country', 'name', 'capacity_mw', 'fuel1', 'commissioning_year']
+        self.dtype_dict = {
+            'country': 'category',
+            'fuel1': 'category',
+            'capacity_mw': 'float32'
+        }
+        self.renewable_sources = ['Hydro', 'Wind', 'Solar', 'Biomass', 'Geothermal', 'Wave and Tidal']
 
-path = "D:\Python_Projects\Projects\Global Power Plant\globalpowerplantdatabasev110\global_power_plant_database.csv"
+    def extract(self):
+        print(f"--- Extracting data from {self.path} ---")
+        try:
+            df = pd.read_csv(self.path, usecols = self.columns, dtype = self.dtype_dict)
+            print(f"Initial Memory Usage: {df.memory_usage(deep = True).sum() / 1024**2:.2f} MB")
+            return df
+        except Exception as e:
+            print(f"Error during extraction: {e}")
+            sys.exit()
 
-columns = ['country', 'name', 'capacity_mw', 'fuel1', 'commissioning_year']
+    def transform(self, df):
+        print("--- Transforming data ---")
+        
+        # Cleaning
+        df['name'] = df['name'].str.strip().str.title()
 
-dtype_dict = {
-    'country': 'category',
-    'fuel1': 'category',
-    'capacity_mw': 'float32'
-}
+        # Contextual Imputation
+        df['commissiong_year'] = df.groupby('fuel1', observed = True)['commissioning_year'].transform(
+            lambda x: x.fillna(x.median())
+        )
 
-df = pd.read_csv(
-    path,
-    usecols = columns,
-    dtype = dtype_dict
-)
+        # Feature Engineering
+        df['is_renewable'] = df['fuel1'].isin(self.renewable_sources)
+        df['renewable_capacity_mw'] = df['capacity_mw'] * df['is_renewable']
 
-print(f"Memory Usage: {df.memory_usage(deep=True).sum() / 1024**2:.2f} MB")
+        return df
+    
+    def load(self, df, table_name):
+        print(f"--- Loading data into table: {table_name} ---")
+        try: 
+            engine = create_engine(self.db_uri)
+            df.to_sql(table_name, engine, if_exists = 'replace', index = False)
+            print("Successfully loaded data to Postgres!")
+        
+        except Exception as e:
+            print(f"Database Error: {e}")
 
+if __name__ == "__main__":
+    CONFIG = {
+        'path': r"D:\Python_Projects\Projects\Global Power Plant\globalpowerplantdatabasev110\global_power_plant_database.csv",
+        'db_uri': "postgresql://postgres:***************@localhost:5432/energy_db"
+    }
 
-# Cleaning & Contextual Imputation
+    etl = PowerPlantETL(CONFIG)
 
-df['name'] = df['name'].str.strip().str.title()
-
-missing = df['commissioning_year'].isnull().sum()
-print(f"Missing years BEFORE: {missing}")
-
-df['commissioning_year'] = df.groupby('fuel1')['commissioning_year'].transform(
-    lambda x: x.fillna(x.median())
-)
-
-missing_after = df['commissioning_year'].isnull().sum()
-print(f"Current Missing Years: {missing_after}")
-
-
-# New Data
-
-renewable_sources = ['Hydro', 'Wind', 'Solar', 'Biomass', 'Geothermal', 'Wave and Tidal']
-
-df['is_renewable'] = df['fuel1'].isin(renewable_sources)
-
-df['renewable_capacity_mw'] = df['capacity_mw'] * df['is_renewable']
-
-country_metrics = (
-    df.groupby('country', observed = True)
-    .agg(
-        total_mw = ('capacity_mw', 'sum'), 
-        renewable_mw = ('renewable_capacity_mw', 'sum')
-    )
-    .assign(
-        renewable_pct = lambda x: (x['renewable_mw'] / x['total_mw']) * 100
-    )
-    .query('total_mw > 1000')
-    .sort_values('renewable_pct', ascending= False)
-    .round(1)
-)
-
-
-# Parquet
-
-output_file = ('cleaned_power_plants.parquet')
-
-df.to_parquet(output_file, engine='pyarrow', index = False)
-
-original_size_mb = os.path.getsize(path) / (1024 * 1024)
-new_size_mb = os.path.getsize(output_file) (1024 * 1024)
-
-print(f"\nOriginal CSV Size: {original_size_mb:.2f} MB")
-print(f"New Parquet Size: {new_size_mb:.2f} MB")
-print(f"Storage Reduced By: {((original_size_mb - new_size_mb) / original_size_mb) * 100:.1f}%")
-
-pd.set_option('display.max_rows', 50)
-pd.set_option('display.max_columns', None)
-
-print("\n--- First 50 rows of the Cleaned Data ---")
-print(df.head(50))
+    raw_df = etl.extract()
+    cleaned_df = etl.transform(raw_df)
+    etl.load(cleaned_df, "power_plants_cleaned")
+    print("\n--- ETL Pipeline Complete")
